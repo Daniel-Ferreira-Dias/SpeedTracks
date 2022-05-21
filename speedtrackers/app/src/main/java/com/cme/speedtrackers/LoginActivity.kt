379318ef77371e3d3f04303dd9359a1e700f9fe5
Daniel Dias <79579779+Daniel-Ferreira-Dias@users.noreply.github.com>
2022-05-21 02:27:16 +0100

    Firebase implemented for login and register

--- speedtrackers/app/src/main/java/com/cme/speedtrackers/LoginActivity.kt
+++ speedtrackers/app/src/main/java/com/cme/speedtrackers/LoginActivity.kt
@@ -1,11 +1,62 @@
 package com.cme.speedtrackers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.cme.speedtrackers.databinding.ActivityLogin2Binding
+import com.cme.speedtrackers.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private var email = ""
+    private var pass = ""
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var binding: ActivityLogin2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login2)
+
+        binding = ActivityLogin2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // Login button
+        binding.loginButton.setOnClickListener {
+            checkConditions()
+        }
+    }
+
+    // System checks if user has the right conditions
+
+    private fun checkConditions() {
+        email = binding.editTextEmail.text.toString()
+        pass = binding.editTextPassword.text.toString()
+
+        if (email.isEmpty()) {
+            Toast.makeText(this, "Insira o seu email...", Toast.LENGTH_SHORT).show()
+        } else if (pass.isEmpty()) {
+            Toast.makeText(this, "Insira a sua password...", Toast.LENGTH_SHORT).show()
+        } else if (email.isNotEmpty() && pass.isNotEmpty()){
+            userLogin()
+        }
+    }
+
+    // System login user
+
+    private fun userLogin(){
+        firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+            if (it.isSuccessful){
+                Toast.makeText(this, "Login com sucesso !", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "Autenticaçãi sem sucesso:" + it.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
--- speedtrackers/app/src/main/java/com/cme/speedtrackers/RegisterActivity.kt
+++ speedtrackers/app/src/main/java/com/cme/speedtrackers/RegisterActivity.kt
@@ -1,11 +1,80 @@
 package com.cme.speedtrackers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.cme.speedtrackers.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private var email = ""
+    private var pass = ""
+    private var confirmPass = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // Already has an account
+        binding.loginHereTextView.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Register
+        binding.registerButton.setOnClickListener {
+            checkConditions()
+        }
+    }
+
+    // System checks if user has the right conditions
+
+    private fun checkConditions() {
+        email = binding.editTextEmail.text.toString()
+        pass = binding.editPassword.text.toString()
+        confirmPass = binding.editPasswordConfirm.text.toString()
+
+        if (email.isEmpty()) {
+            Toast.makeText(this, "Insira o seu email...", Toast.LENGTH_SHORT).show()
+        } else if (pass.isEmpty()) {
+            Toast.makeText(this, "Insira a sua password...", Toast.LENGTH_SHORT).show()
+        } else if (confirmPass.isEmpty()) {
+            Toast.makeText(this, "Insira a confirmação da password", Toast.LENGTH_SHORT).show()
+        } else if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+            if (pass == confirmPass) {
+                registerUser()
+            } else {
+                Toast.makeText(this, "As passwords não coincidem", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    // System register user information
+
+    private fun registerUser() {
+        firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Utilizador registado!", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(
+                    this,
+                    "Algo correu mal: " + it.exception.toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }
     }
 }

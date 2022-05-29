af4ab1aa8b256c62cd115aa40c4effc7cafef439
Pmct17 <79578173+Pmct17@users.noreply.github.com>
2022-05-29 22:35:09 +0100

    Splashscreen

--- speedtrackers/app/src/main/java/com/cme/speedtrackers/SplashScreenActivity.kt
+++ speedtrackers/app/src/main/java/com/cme/speedtrackers/SplashScreenActivity.kt
@@ -3,47 +3,35 @@ package com.cme.speedtrackers
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.cme.speedtrackers.databinding.ActivitySplashScreenBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class SplashScreenActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySplashScreenBinding
-    private lateinit var auth: FirebaseAuth
-    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var  firebaseAuth : FirebaseAuth
 
-    //lateinit var gv : GlobalClass
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
-        auth = FirebaseAuth.getInstance()
-        //gv = application as GlobalClass
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        Handler().postDelayed({
+            checkUser()
+        }, 3000)
 
     }
 
-    override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        if (currentUser != null) {
-            val email = currentUser.email!!
-            //gv.emailUtilizador = email
-            val uid = currentUser.uid
-
-
-
-            //val nome = getNome(uid)
-            //gv.uidUtilizador = uid
-
-
-            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+    private fun checkUser(){
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser == null){
+            // user not logged
+            startActivity(Intent(this, WelcomeActivity::class.java))
             finish()
-
-
-        } else {
-            startActivity(Intent(this@SplashScreenActivity, WelcomeActivity::class.java))
-
-
+        } else{
+            startActivity(Intent(this, BottomNavigationActivity::class.java))
+            finish()
         }
-
     }
 }
